5bf7fb7118e99bda1a8818273f0a7c24177a9d14
J-Ghodasara <jay.ghodasara.sa@gmail.com>
2018-07-10 19:12:02 +0530

    first commit

---
+++ app/src/main/java/com/jayghodasara/inappbilling/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.jayghodasara.inappbilling
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.android.billingclient.api.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), PurchasesUpdatedListener {
+
+    lateinit var billingClient:BillingClient
+    var hashMap:HashMap<String,String> = HashMap()
+    var hashMap2:HashMap<String,String> = HashMap()
+
+    override fun onPurchasesUpdated(responseCode: Int, purchases: MutableList<Purchase>?) {
+        if (responseCode == BillingClient.BillingResponse.OK && purchases != null) {
+            for (purchase in purchases) {
+                // handlePurchase(purchase)
+
+                billingClient.consumeAsync(purchase.purchaseToken) { responseCode, purchaseToken -> Toast.makeText(this,"Consumed", Toast.LENGTH_LONG).show() }
+            }
+        } else if (responseCode == BillingClient.BillingResponse.USER_CANCELED) {
+            // Handle an error caused by a user cancelling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
+    }
+
+    var list:ArrayList<String> = ArrayList()
+
+
+    fun queryskudetails(){
+
+        billingClient=BillingClient.newBuilder(this).setListener(this).build()
+        billingClient.startConnection(object: BillingClientStateListener {
+            override fun onBillingServiceDisconnected() {
+                Log.i("Disconnected","billing client")
+            }
+
+            override fun onBillingSetupFinished(responseCode: Int) {
+
+                billingClient.let { billingClient ->
+
+                    val skulist = ArrayList<String>()
+                    skulist.add("subscribe")
+
+                    val params= SkuDetailsParams.newBuilder()
+                    params.setSkusList(skulist).setType(BillingClient.SkuType.SUBS)
+                    billingClient.querySkuDetailsAsync(params.build(),{
+                        responseCode, skuDetailsList ->
+
+                        if(responseCode== BillingClient.BillingResponse.OK && skuDetailsList !=null){
+                            for (skuDetails in skuDetailsList) {
+                                val sku = skuDetails.sku
+                                val price = skuDetails.price
+                                Log.i("skudetails",sku)
+                                Log.i("skuprice",price)
+                                hashMap[sku] = price
+
+
+                            }
+                        }
+
+                    })
+                }
+
+//
+//                if(responseCode == BillingClient.BillingResponse.OK){
+//                    Toast.makeText(context,"setup finished",Toast.LENGTH_LONG).show()
+//
+//
+//                }
+            }
+
+        })
+    }
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        list.add("Books")
+        list.add("Pens")
+        list.add("Keychains")
+        list.add("Mobiles")
+        queryskudetails()
+        subscribe.setOnClickListener(View.OnClickListener {
+
+
+
+            val flowParams = BillingFlowParams.newBuilder()
+                    .setSku("subscribe")
+                    .setType(BillingClient.SkuType.SUBS) // SkuType.SUB for subscription
+
+                    .build()
+            val responseCode = billingClient.launchBillingFlow(this, flowParams)
+        })
+
+        var layman:RecyclerView.LayoutManager= LinearLayoutManager(this,LinearLayout.VERTICAL,false)
+
+        var adapter=myadapter(this,list)
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager=layman
+
+
+    }
+}
---
+++ app/src/main/java/com/jayghodasara/inappbilling/myadapter.kt
@@ -0,0 +1,166 @@
+package com.jayghodasara.inappbilling
+
+import android.app.Activity
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.android.billingclient.api.*
+import com.android.billingclient.util.BillingHelper
+
+class myadapter(var context:Context,var array:ArrayList<String>): RecyclerView.Adapter<myadapter.Vholder>(), PurchasesUpdatedListener {
+
+    lateinit var billingClient:BillingClient
+    var hashMap:HashMap<String,String> = HashMap()
+    var hashMap2:HashMap<String,String> = HashMap()
+
+    override fun onPurchasesUpdated(responseCode: Int, purchases: MutableList<Purchase>?) {
+        if (responseCode == BillingClient.BillingResponse.OK && purchases != null) {
+            for (purchase in purchases) {
+                // handlePurchase(purchase)
+
+                        billingClient.consumeAsync(purchase.purchaseToken) { responseCode, purchaseToken -> Toast.makeText(context,"Consumed",Toast.LENGTH_LONG).show() }
+            }
+        } else if (responseCode == BillingClient.BillingResponse.USER_CANCELED) {
+            // Handle an error caused by a user cancelling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
+    }
+
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vholder {
+        var v:View= LayoutInflater.from(parent.context).inflate(R.layout.items,parent,false)
+
+
+
+
+
+        return Vholder(v)
+    }
+//    fun startconnection(run:() -> Unit){
+//        billingClient.startConnection(object : BillingClientStateListener {
+//            override fun onBillingSetupFinished(responseCode: Int) {
+//                if (responseCode == BillingClient.BillingResponse.OK) {
+//                    // isBillingConnected = true
+//                    // The billing client is ready. You can query purchases here.
+//                    run()
+//                }
+//            }
+//
+//            override fun onBillingServiceDisconnected() {
+//                // Try to restart the connection on the next request to
+//                // Google Play by calling the startConnection() method.
+//                // isBillingConnected = false
+//            }
+//        })
+//    }
+    fun queryskudetails(){
+
+        billingClient=BillingClient.newBuilder(context).setListener(this).build()
+        billingClient.startConnection(object:BillingClientStateListener{
+            override fun onBillingServiceDisconnected() {
+                Log.i("Disconnected","billing client")
+            }
+
+            override fun onBillingSetupFinished(responseCode: Int) {
+
+                billingClient.let { billingClient ->
+
+                    val skulist = ArrayList<String>()
+                    skulist.add("books")
+                    skulist.add("pens")
+                    skulist.add("keychains")
+
+                    val params= SkuDetailsParams.newBuilder()
+                    params.setSkusList(skulist).setType(BillingClient.SkuType.INAPP)
+                    billingClient.querySkuDetailsAsync(params.build(),{
+                        responseCode, skuDetailsList ->
+
+                        if(responseCode== BillingClient.BillingResponse.OK && skuDetailsList !=null){
+                            for (skuDetails in skuDetailsList) {
+                                val sku = skuDetails.sku
+                                val price = skuDetails.price
+                                Log.i("skudetails",sku)
+                                Log.i("skuprice",price)
+                                hashMap[sku] = price
+
+
+                            }
+                        }
+
+                    })
+                }
+
+//
+//                if(responseCode == BillingClient.BillingResponse.OK){
+//                    Toast.makeText(context,"setup finished",Toast.LENGTH_LONG).show()
+//
+//
+//                }
+            }
+
+        })
+    }
+
+
+
+
+    override fun getItemCount(): Int {
+       return array.size
+    }
+
+
+
+
+    override fun onBindViewHolder(holder: Vholder, position: Int) {
+        queryskudetails()
+
+        var text:String= array[position]
+        Log.i("text",text)
+        holder.textView.text=text
+        holder.Price.text=hashMap[text.toLowerCase()].toString()
+        Log.i("price",hashMap["books"].toString())
+        holder.btn.setOnClickListener(View.OnClickListener {
+            Log.i("button",text.toLowerCase())
+            var skuid=hashMap2[text]
+            val flowParams = BillingFlowParams.newBuilder()
+                    .setSku(text.toLowerCase())
+                    .setType(BillingClient.SkuType.INAPP) // SkuType.SUB for subscription
+
+                    .build()
+            val responseCode = billingClient.launchBillingFlow(context as Activity?, flowParams)
+
+
+        })
+    }
+//    fun executeRequest(run: () -> Unit) {
+//        if (billingClient.isReady) {
+//            run()
+//        }
+//        else {
+//            startconnection(run)
+//        }
+//    }
+
+
+
+
+
+
+    inner class Vholder(itemView:View): RecyclerView.ViewHolder(itemView) {
+
+     var textView:TextView = itemView.findViewById(R.id.textView1)
+        var btn:ImageView= itemView.findViewById(R.id.add_to_cart)
+        var Price:TextView= itemView.findViewById(R.id.price)
+
+    }
+}
